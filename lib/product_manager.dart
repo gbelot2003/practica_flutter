45388192402279af1782cbb5a6e5fd80220be370
Gerardo Belot <gbelot2003@hotmail.com>
2019-05-22 07:05:48 -0600

    Creando un control para el boton, migrandolo y accediendo a funcion desde ese control

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -32,21 +33,19 @@ class _productManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product){
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('ProductsManager State Build');
     return Column(children: <Widget>[
       Container(
         margin: EdgeInsets.all(10.0),
-        child: RaisedButton(
-          color: Theme.of(context).primaryColor,
-          onPressed: () {
-            setState(() {
-              _products.add('Advance Foot Tester');
-            });
-          },
-          child: Text("Add Product"),
-        ),
+        child: ProductControl(_addProduct),
       ),
       Products(_products)
     ]);
